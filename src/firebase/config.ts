9dec12f9e7c5b8c29c519ca8df1d144746e93df7
Himank Bhardwaj <himankbhardwaj21@gmail.com>
2025-11-05 16:19:23 +0530

    Delete src/firebase/config.ts

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-1852665342-f0962",
-  "appId": "1:1021271568563:web:92345835c28c86e34500c6",
-  "apiKey": "",
-  "authDomain": "studio-1852665342-f0962.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1021271568563"
-};
